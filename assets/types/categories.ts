8d5a240bd80ddfdb35543ad6293e407630ccd714
Valeria Alcaraz Areco <vale.alcaraz14@gmail.com>
2021-08-13 12:47:08 -0400

    working on remove category

--- assets/types/categories.ts
+++ assets/types/categories.ts
@@ -1,7 +1,6 @@
 const newCatForm = document.getElementById ("newcat-form");
 const inputCat = document.getElementById ("input-cat");
 const removeBtns = document.getElementsByClassName ("remove-cat");
-console.log(removeBtns)
 
 // Object "categories"
 
@@ -12,10 +11,12 @@ const categories : Category = {
 
 // Create new category
 
-const categoryCont = document.getElementById("cont-category");
-const categoryList = ["comida","servicios","salidas","educacion","transporte","trabajo"];
+const categoriesGrid = document.getElementById("categories-grid");
 
 const createCategoryRow = (name) => {
+    
+    categoriesGrid.innerHTML = " "; //QUEDA PENDIENTE! 
+    
     
     let categoryText = document.createTextNode(name);
     const categoryDiv = document.createElement("div");
@@ -26,43 +27,55 @@ const createCategoryRow = (name) => {
     const editLink = document.createTextNode("Editar");
     const removeLink = document.createTextNode("Eliminar");
 
-    categoryCont.appendChild(categoryDiv);
+    categoriesGrid.appendChild(categoryDiv);
+
     categoryDiv.classList.add("row");
     categoryDiv.classList.add("mt-3");
     categoryDiv.appendChild(divNewCat);
     categoryDiv.appendChild(divActions);
     categoryDiv.setAttribute("id",generateId(10));
+
     divNewCat.appendChild(categoryText);
+    divNewCat.dataset.name = name;
     divNewCat.classList.add("fw-bold");
     divNewCat.classList.add("col-8");
+
     divActions.appendChild(editCat);
     divActions.appendChild(removeCat);
     divActions.classList.add("col-4");
     divActions.classList.add("d-flex");
     divActions.classList.add("justify-content-around");
+
     editCat.appendChild(editLink);
     editCat.setAttribute("href","./edit_cat.html");
     editCat.classList.add("text-success");
+
     removeCat.appendChild(removeLink);
     removeCat.setAttribute("href","#"); // Hacer despues funcion que remueva
     removeCat.setAttribute("class","text-danger remove-cat"); 
+    console.log("funciona")
+
+    const storage = getStorage();
+    
+
 }
 
 // Show new categories on screen / Set Local Storage new data:
 
 newCatForm.addEventListener('submit',(e) => {
 
-    const storage = getStorage();
-
     e.preventDefault();
-          
+
+    const storage = getStorage();
+    
     categories.name = inputCat.value;
     categories.slug = slugify(inputCat.value);
-
+    
     storage.categories.push(categories);
-    localStorage.setItem('to-storage', JSON.stringify(storage));
-
+    
     createCategoryRow(categories.name);
+    
+    localStorage.setItem('to-storage', JSON.stringify(storage));
 
     newCatForm.reset();
 
@@ -80,30 +93,33 @@ const init = () => {
 
 init()
 
-
 // Remove categories from screen:
 
-const removeFunction = removeBtns => {
+const removeFunction = removeBtns => {   
     for(let button of removeBtns) {
-
         let divButton = button.parentElement;
         let divRow = divButton.parentElement;
-
+        const targetName = divRow.firstChild;
+        let dataName = targetName.getAttribute("data-name");
         button.addEventListener("click", e => {            
-            divButton.remove();
-            divRow.remove();
-            // localStorage.removeItem('categories');
-
-        }
+            const storage = getStorage();
+            const {categories} = storage;            
+            const categoriesUpdate = categories.filter(category => dataName !== category.name)
+            for(let category of categories){
+                createCategoryRow(category.name)
+            }           
+            localStorage.setItem('to-storage', JSON.stringify({...storage, categories: categoriesUpdate}));
+        })
     }
 }
 
 removeFunction(removeBtns)
 
+// localStorage.removeItem('categories');
+
+
 // Remove categories from Local Storage:
 // to-storage
-
-
 
 
 
