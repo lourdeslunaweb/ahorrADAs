75d3cb46c243dfd0853305d7c8bde06e51f88285
Lourdes Luna <67447466+lourdeslunaweb@users.noreply.github.com>
2021-08-21 23:57:05 -0300

    edit and remove operation done

--- assets/types/edit-operation.js
+++ assets/types/edit-operation.js
@@ -6,27 +6,26 @@ var amountToEditValue = params.get("amountOp");
 var typeToEditValue = params.get("typeOp");
 var categoryToEditValue = params.get("categoryOp");
 var dateToEditValue = params.get("dateOp");
-// Create nodes with id
+// Create nodes with id - Node category is already created as newOpCategory (operations.ts)
 var descToEdit = document.getElementById("desc-to-edit");
 var amountToEdit = document.getElementById("amount-to-edit");
 var typeToEdit = document.getElementById("type-to-edit");
 var dateToEdit = document.getElementById("date-to-edit");
 var editOpBtn = document.getElementById("edit-op-btn");
-var categoryToEdit = document.getElementById("category-to-edit");
 // Set values from get params
 descToEdit.value = "" + descToEditValue;
 amountToEdit.value = "" + amountToEditValue;
 typeToEdit.value = "" + typeToEditValue;
-categoryToEdit.value = "" + categoryToEditValue;
+newOpCategory.value = "" + categoryToEditValue;
 dateToEdit.value = "" + dateToEditValue;
 //Save old values in variables
 var oldDescription = descToEdit.value;
 var oldAmount = amountToEdit.value;
 var oldType = typeToEdit.value;
-var oldCategory = categoryToEdit.value;
+var oldCategory = newOpCategory.value;
 var oldDate = dateToEdit.value;
 // Up Date the categories of the operations
-// upDateCatOps()
+upDateCatOps();
 // Edit Operation
 editOpBtn.addEventListener("click", function (e) {
     e.preventDefault();
@@ -36,27 +35,18 @@ editOpBtn.addEventListener("click", function (e) {
     var newDescription = descToEdit.value;
     var newAmount = amountToEdit.value;
     var newType = typeToEdit.value;
-    var newCategory = categoryToEdit.value;
+    var newCategory = newOpCategory.value;
     var newDate = dateToEdit.value;
     for (var _i = 0, operations_1 = operations; _i < operations_1.length; _i++) {
         var operation = operations_1[_i];
         if (operation.description === oldDescription) {
             operation.description = newDescription;
-        }
-        else if (operation.amount === oldAmount) {
             operation.amount = newAmount;
-        }
-        else if (operation.type === oldType) {
             operation.type = newType;
-        }
-        else if (operation.category === oldCategory) {
             operation.category = newCategory;
-        }
-        else if (operation.date === oldDate) {
             operation.date = newDate;
         }
     }
     localStorage.setItem('to-storage', JSON.stringify(storage));
-    // refreshOperationTable()
-    // window.location.href = "./index.html"
+    window.location.href = "./index.html";
 });
--- assets/types/edit-operation.ts
+++ assets/types/edit-operation.ts
@@ -6,56 +6,49 @@ const amountToEditValue = params.get("amountOp");
 const typeToEditValue = params.get("typeOp");
 const categoryToEditValue = params.get("categoryOp");
 const dateToEditValue = params.get("dateOp");
-// Create nodes with id
+// Create nodes with id - Node category is already created as newOpCategory (operations.ts)
 const descToEdit = document.getElementById("desc-to-edit");
 const amountToEdit = document.getElementById("amount-to-edit");
 const typeToEdit = document.getElementById("type-to-edit");
 const dateToEdit = document.getElementById("date-to-edit");
 const editOpBtn = document.getElementById("edit-op-btn");
-const categoryToEdit = document.getElementById("category-to-edit");
 // Set values from get params
 descToEdit.value = `${descToEditValue}`;
 amountToEdit.value = `${amountToEditValue}`;
 typeToEdit.value = `${typeToEditValue}`;
-categoryToEdit.value = `${categoryToEditValue}`;
+newOpCategory.value = `${categoryToEditValue}`;
 dateToEdit.value = `${dateToEditValue}`;
 //Save old values in variables
 let oldDescription = descToEdit.value;
 let oldAmount = amountToEdit.value;
 let oldType = typeToEdit.value;
-let oldCategory = categoryToEdit.value;
+let oldCategory = newOpCategory.value;
 let oldDate = dateToEdit.value;
 
 
 // Up Date the categories of the operations
-// upDateCatOps()
+upDateCatOps()
 
 // Edit Operation
 editOpBtn.addEventListener("click", e => {
     e.preventDefault()
     console.log("click editar")
     const storage = getStorage();
-    const {operations} = storage;
+    const { operations } = storage;
     let newDescription = descToEdit.value;
     let newAmount = amountToEdit.value;
     let newType = typeToEdit.value;
-    let newCategory = categoryToEdit.value;
+    let newCategory = newOpCategory.value;
     let newDate = dateToEdit.value;
-    for(let operation of operations) {
-        if(operation.description === oldDescription) {
+    for (let operation of operations) {
+        if (operation.description === oldDescription) {
             operation.description = newDescription;
-        }
-        else if(operation.amount === oldAmount){
             operation.amount = newAmount;
-        } else if (operation.type === oldType ){
             operation.type = newType;
-        } else if (operation.category === oldCategory){
-            operation.category = newCategory
-        } else if (operation.date === oldDate){
+            operation.category = newCategory;
             operation.date = newDate;
         }
     }
     localStorage.setItem('to-storage', JSON.stringify(storage));
-    // refreshOperationTable()
-    // window.location.href = "./index.html"
+    window.location.href = "./index.html"
 })
